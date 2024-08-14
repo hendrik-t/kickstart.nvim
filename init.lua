480ea4542e9d92cdf581f7d32c001ed626f680d4
Henrio <25921516+hendrik-t@users.noreply.github.com>
2024-08-14 23:38:51 +0200

    Set tab width to 4

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Set Tab Width to 4 spaces
+vim.opt.tabstop = 4
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
